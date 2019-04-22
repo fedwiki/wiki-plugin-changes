323e0e773329f736f41f14bba2e22bd6dd5f3708
Paul Rodwell <paul.rodwell@btinternet.com>
2019-04-22 16:22:42 +0100

    add babel transpile for latest coffeescript and older browser support

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,7 @@ module.exports = function (grunt) {
       client: {
         expand: true,
         options: {
+          transpile: [],
           sourceMap: true
         },
         src: ['client/*.coffee', 'test/*.coffee'],
